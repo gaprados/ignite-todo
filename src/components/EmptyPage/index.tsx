d8f98cac141826e79d4eb1f1bdbd9f5404b5ddd3
Gabriel Prado <gaprados3@gmail.com>
2022-06-13 20:45:29 -0300

    feat: styled empty page component

--- src/components/EmptyPage/index.tsx
+++ src/components/EmptyPage/index.tsx
@@ -3,7 +3,9 @@ import styles from './EmptyPage.module.css';
 export function EmptyPage() {
   return (
     <div className={styles.container}>
-      <h1>EmptyPage</h1>
+      <img src='/src/assets/clipboard.svg' alt='Clipboard' />
+      <strong>Você ainda não tem tarefas cadastradas</strong>
+      <span>Crie tarefas e organize seus itens a fazer</span>
     </div>
   );
 }
